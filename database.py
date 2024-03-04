ba4e2ed1c0e34ec5b1b237b16738649e4575daaa
Akaikumogo <98486553+Akaikumogo@users.noreply.github.com>
2024-03-04 15:35:50 +0500

    aaaaa

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 
-DATABASE_URL ='sqlite:///./users.db'
+DATABASE_URL ='sqlite:///./data/users.db'
 engine = create_engine(DATABASE_URL,connect_args={"check_same_thread":False})
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base=declarative_base()
